e64caf38662424f19739b4856b462cf56649e9aa
orekyuu <orekyuu@yahoo.co.jp>
2018-04-29 17:17:34 +0900

    SqlBindingParamをジェネリクスでいい感じにする

--- feeling/src/main/java/net/orekyuu/moco/feeling/node/SqlBindParam.java
+++ feeling/src/main/java/net/orekyuu/moco/feeling/node/SqlBindParam.java
@@ -3,25 +3,17 @@ package net.orekyuu.moco.feeling.node;
 import net.orekyuu.moco.feeling.SqlContext;
 import net.orekyuu.moco.feeling.visitor.SqlVisitor;
 
-public class SqlBindParam implements SqlNode {
-    private final Class<?> clazz;
-    private final Object value;
+public class SqlBindParam<T> implements SqlNode {
+    private final Class<T> clazz;
+    private final T value;
 
-    public SqlBindParam(Object value, Class<?> clazz) {
-        if (!clazz.isInstance(value)) {
-            throw new ClassCastException();
-        }
+    public SqlBindParam(T value, Class<T> clazz) {
         this.value = value;
         this.clazz = clazz;
     }
 
-    public Object getValue() {
+    public T getValue() {
         return value;
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T> T getValue(Class<T> clazz) {
-        return (T) value;
     }
 
     public Class<?> getClazz() {
